8907f29f1de293e8ba7bbfac392f15928e56c3f6
Bryson <bryce.akau08@gmail.com>
2018-08-23 15:52:13 -0700

    need to get done

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,6 @@
+import { createStore } from 'redux';
 import reducer from './reducer';
-import reduxPromiseMiddleware from 'redux-promise-middleware';
 
-export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+export default store;
